5adc49029c5e8826793303ab37a7c029e1119ab2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-08-12 17:25:10 -0700

    Finished show balance status - to do: Show Transactions

--- trials.js
+++ trials.js
@@ -8,41 +8,90 @@ let businessName = "Balloonicorn's Cupcake Shop";
 
 const addresses = ["123 Main Street", "683 Sutter Street", "1600 Pennsylvania Ave"];
 
-const phoneNumbers = new Map();
-
-// Add some phone numbers to our map
-
-
+const phoneNumbers = new Map([
+    ['home', '510-867-5309'],
+    ['mobile', '415-555-1212'],
+    ['business', '415-123-4567']
+]);
 
 // ///////////////////////////////////////////////////////
 // Create User Info Display:
 
-// Add function to print account information 
+// Add function to print account information
+function accountInfo(accountHolder, accountNumber, businessName) {
+  console.log (`Account Holder Name: ${accountHolder}`);
+  console.log (`Account Holder Number: ${accountNumber}`);
+  console.log (`Business Name: ${businessName}`);
+}
+
+accountInfo("Balloonicorn", 123456, "Balloonicorn's Cupcake Shop");
 
 
 // Add function to print all addresses, including a header
+function showAddresses(addresses){
+  console.log(`Addesses:`)
+  for (let address of addresses) {
+    console.log(address);
+  }
+}
+showAddresses(["123 Main Street", "683 Sutter Street", "1600 Pennsylvania Ave"]);
 
 
 // Add function to print phone types and numbers
+function showPhoneNums(phoneNumbers) {
+  console.log(`Phone Numbers:`)
+  for (let keyValue of phoneNumbers) {
+    console.log (
+    `${keyValue[0]}: ${keyValue[1]}`
+    );
+  }
+}
 
+showPhoneNums(phoneNumbers);
 
 // ///////////////////////////////////////////////////////
 // Transactions:
 
 // Create an empty map of transactions
-
+const transactions = new Map();
 
 // Add function to add transactions
-
+function addTransaction(date, amount) {
+  transactions.set(date, amount);
+}
 
 // Use the function to add transactions
-
+addTransaction('May-2', -500);
+addTransaction('May-13', +1200);
+addTransaction('May-15', -100);
+addTransaction('May-21', -359);
+addTransaction('May-29', +2200);
 
 // Add function to show balance status
-
+function showBalanceStatus(balanceAmount) {
+  console.log(`Balance: ${balanceAmount}`);
+  if (balanceAmount < 0) {
+    console.log ("YOU ARE OVERDRAWN");
+} else if (balanceAmount > 0 && balanceAmount < 20) {
+    console.log("Warning: You are close to zero balance");
+} else{
+    console.log("Thank you for your business");
+  }
+}
 
 // Add function to show transactions
+function showTransactions(transactions, startingBalance) {
+  console.log(`Starting Balance: ${startingBalance}`);
+  for (keyValue of transactions){
 
+  }
+}
+
+
+  // for (let keyValue of phoneNumbers) {
+  //   console.log (
+  //   `${keyValue[0]}: ${keyValue[1]}`
+  //   );
 
 // ///////////////////////////////////////////////////////
 // All Customer Info:
@@ -81,7 +130,7 @@ const phoneNumbers = new Map();
 // Write a function to return the address of a given person
 
 
-// Add a function to create an employee schedule for the week 
+// Add a function to create an employee schedule for the week
 
 
 // Add a function for weekly hours
